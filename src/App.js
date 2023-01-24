2fe42bae07046a66a9ebb1c6075bd6ed8c65360d
EvaGostiuk <egost073@uottawa.ca>
2023-01-24 01:22:26 -0500

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import ForceGraph3D from "3d-force-graph";
+import nodes from './nodes.json';
+import links from './links.json';
 
-function App() {
+
+const GraphGenerator = (props) => {
+  useEffect(() => {
+    const ele = document.getElementById("my-graph");
+    const myGraph = ForceGraph3D();
+    const myData = {
+      nodes, links
+    };
+
+    let hoverNode = null;
+
+    myGraph(ele).graphData(myData)
+    .nodeLabel(node => `${node.title}`)
+    .nodeColor(node => node === hoverNode ? '#e5edf3' : '#7ba5ec')
+    .onNodeClick(node => window.open(`https://arxiv.org/abs/${node.id}`, '_blank'))
+    .onNodeHover(node => {
+      // no state change
+      if (!node  || (node && hoverNode === node)) return;
+      hoverNode = node || null;
+
+      updateHighlight();
+    });
+
+    function updateHighlight() {
+      // trigger update of highlighted objects in scene
+      myGraph
+        .nodeColor(myGraph.nodeColor())
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="my-graph"></div>
+    </>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <div className="App">
+      <div class="graph">
+        <GraphGenerator />
+      </div>
+      <div class="sidebar">
+        
+          <h1>2023 Research In AI</h1>
+
+          <p>This graph is the visualization of the relationships between papers which were submitted in 2023 to Artificial Intelligence section of https://arxiv.org/ .</p>
+          <p> You can hover over the node to see titile of publication, while clicking on any node will lead you to the information card of the corresponding publication.</p>
+        
+      </div>
+    </div>
+  );
+}
